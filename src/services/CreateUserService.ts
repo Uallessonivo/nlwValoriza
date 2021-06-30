680cab41574b3fead8b4e9507744e99160f3ddf0
Uallesson <uallessons@gmail.com>
2021-06-30 09:37:32 -0300

    ..

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,34 @@
+import { getCustomRepository } from "typeorm"
+import { UserRepositories } from "../repositories/UserRepositories"
+
+
+interface IUserRequest {
+    name: string;
+    email: string;
+    admin?: boolean;
+}
+
+class CreateUserService {
+
+    async execute({ name, email, admin }: IUserRequest) {
+        const usersRepository = getCustomRepository(UserRepositories)
+
+        if (!email) {
+            throw new Error("E-mail incorrect")
+        }
+
+        const userAlreadyExists = await usersRepository.findOne({ email })
+
+        if (userAlreadyExists) {
+            throw new Error("User already exists")
+        }
+
+        const user = usersRepository.create({ name, email, admin })
+
+        await usersRepository.save(user)
+        return user
+    }
+
+}
+
+export { CreateUserService }
